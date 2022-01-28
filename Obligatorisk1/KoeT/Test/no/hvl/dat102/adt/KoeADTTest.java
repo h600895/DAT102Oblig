45b510b1b9afa03153127c47d045f53149f054d0
h600895 <600895@stud.hvl.no>
2022-01-28 11:27:57 +0100

    Oppgave4 ferdig. Må sjekkes

--- Obligatorisk1/KoeT/Test/no/hvl/dat102/adt/KoeADTTest.java
+++ Obligatorisk1/KoeT/Test/no/hvl/dat102/adt/KoeADTTest.java
@@ -6,6 +6,113 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class KoeADTTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class KoeADTTest {
+	//Referanse til koeObjektet
+	private KoeADT<Integer> koe;
+	
+	//Testdata
+	private Integer e1 = 1;
+	private Integer e2 = 2;
+	private Integer e3 = 3;
+	private Integer e4 = 4;
+	private Integer e5 = 5;
+	
+	protected abstract KoeADT<Integer> reset();
+	
+	/**
+	 * Hent en ny stabel for hver test.
+	 * 
+	 * @return
+	 */
+	@BeforeEach
+	public void setup() {
+		koe = reset();
+	}
+	@Test
+	public void nyKoeErTom() {
+		assertTrue(koe.erTom());
+	}
+	@Test
+	public void innOgUtOgForsteKoe() {
+		koe.innKoe(e1);
+		koe.innKoe(e2);
+		koe.innKoe(e3);
+		
+		try {
+			assertEquals(e1, koe.foerste());
+			assertEquals(e1, koe.utKoe());
+			assertEquals(e2, koe.foerste());
+			assertEquals(e2, koe.utKoe());
+			assertEquals(e3, koe.foerste());
+			assertEquals(e3, koe.utKoe());
+		} catch (EmptyCollectionException e) {
+			fail("utKoe filet uventet" + e.getMessage());
+		}
+	}
+	@Test
+	public void innOgUtKoeOgForsteDuplikat() {
+		koe.innKoe(e1);
+		koe.innKoe(e2);
+		koe.innKoe(e2);
+		koe.innKoe(e3);
+		
+		try {
+			assertEquals(e1, koe.foerste());
+			assertEquals(e1, koe.utKoe());
+			assertEquals(e2, koe.foerste());
+			assertEquals(e2, koe.utKoe());
+			assertEquals(e2, koe.foerste());
+			assertEquals(e2, koe.utKoe());
+			assertEquals(e3, koe.foerste());
+			assertEquals(e3, koe.utKoe());
+		} catch (EmptyCollectionException e) {
+			fail("utKoe filet uventet" + e.getMessage());
+		}
+	}
+	@Test
+	public void utOgErTom() {
+		koe.innKoe(e1);
+		koe.innKoe(e2);
+		koe.innKoe(e3);
+		
+		try {
+			System.out.println(koe.utKoe());
+			//assertEquals(e1, koe.foerste());
+			assertEquals(e2, koe.utKoe());
+			assertEquals(e3, koe.utKoe());
+		} catch (EmptyCollectionException e) {
+			fail("utKoe filet uventet" + e.getMessage());
+		}
+		assertTrue(koe.erTom());
+	}
+	@Test
+	public void erIkkeTom() {
+		koe.innKoe(e1);
+		assertFalse(koe.erTom());
+		koe.innKoe(e2);
+		assertFalse(koe.erTom());
+	}
+	public void innUtErTom() {
+		try {
+			koe.innKoe(e1);
+			koe.utKoe();
+			assertTrue(koe.erTom());
+		} catch (EmptyCollectionException e) {
+			fail("Inn- eller ut- koe feilet uventet" + e.getMessage());
+		}
+	}
+	public void utKoeEmptryIsUnderflowed() {
+		Assertions.assertThrows(EmptyCollectionException.class, () -> {
+			koe.utKoe();
+		});
+	}
 }
