baf7b02ee54ea484519645d1078d83b141dcfd8a
Nora <600891@stud.hvl.no>
2022-03-01 13:50:20 +0100

    refactor

--- Obligatorisk2/MengdeU/src/no/hvl/dat102/hobby/Tekstgrensesnitt.java
+++ Obligatorisk2/MengdeU/src/no/hvl/dat102/hobby/Tekstgrensesnitt.java
@@ -2,11 +2,7 @@ package no.hvl.dat102.hobby;
 
 import java.util.Scanner;
 
-import no.hvl.dat102.hobby.Datakontakt;
 import no.hvl.dat102.mengde.adt.MengdeADT;
-import no.hvl.dat102.hobby.Hobby;
-import no.hvl.dat102.hobby.Medlem;
-import no.hvl.dat102.hobby.Tekstgrensesnitt;
 import no.hvl.dat102.mengde.tabell.TabellMengde;
 
 public class Tekstgrensesnitt extends Datakontakt{
